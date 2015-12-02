6f3641e379fd8ad907e5aacfed6f1ccd6147f75b
andreaskapou <kapouranis.andreas@gmail.com>
2015-12-02 10:42:59 +0000

    Finish the rbf design matrix

--- R/design_matrix.R
+++ R/design_matrix.R
@@ -77,7 +77,7 @@ design_matrix.polynomial <- function(x, obs, ...){
 #' @seealso \code{\link{design_matrix}}, \code{\link{rbf_basis}}
 #'
 #' @examples
-#' obj <- polynomial.object(M=2)
+#' obj <- rbf.object(M=2)
 #' obs <- c(0,.2,.5)
 #' H <- design_matrix(obj, obs)
 #'
@@ -87,15 +87,35 @@ design_matrix.rbf <- function(x, obs, ...){
   assertthat::assert_that(is.vector(obs))
 
   ## TODO Compute the centers using K-means!!!
-
+  mus <- NULL
   N   <- length(obs)  # Length of the dataset
-  M   <- x$M + 1      # Number of coefficients
-  gamma <- x$gamma    # Inverse width of basis function
-  H <- matrix(1, nrow = N, ncol = M)
-  # Convert the 'obs' vector to an N x 1 dimensional matrix
-  obs <- as.matrix(obs)
-  for (j in 1:M){
-    H[ ,j] <- apply(obs, 1, rbf_basis, mus = M, gamma = gamma)
+  M   <- x$M          # Number of coefficients
+  # In case the number of basis functions is higher than the total observations
+  # set the number of basis functions equal to the observations.
+  # TODO: Check that this holds!
+  if (M > N){
+    M <- N
   }
-  return(list(H = H))
+  if (M == 0){
+    H <- matrix(1, nrow = N, ncol = M + 1)
+  }else{
+    # Convert the 'obs' vector to an N x 1 dimensional matrix
+    obs <- as.matrix(obs)
+    gamma <- x$gamma  # Inverse width of basis function
+
+    repeat {
+      km <- stats::kmeans(obs, M, nstart = 10)  # Use K-means
+      if (min(km$size) > 0)  # Only accept non-empty clusters
+        break
+    }
+    mus <- km$centers  # RBF centers
+
+    H <- matrix(1, nrow = N, ncol = M)
+    for (j in 1:M){
+      # TODO: Implement the multivariate case!
+      H[ ,j] <- apply(obs, 1, rbf_basis, mus = mus[j], gamma = gamma)
+    }
+    H <- cbind (1 , H)  # Add the 'bias' term
+  }
+  return(list(H = H, mus = mus))
 }
