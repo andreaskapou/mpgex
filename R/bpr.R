5deb4384eff9d2f0b0a59c9f183488832a49192a
andreaskapou <kapouranis.andreas@gmail.com>
2016-02-02 19:09:29 +0000

    Implement sum of BPR likelihood functions

--- R/bpr.R
+++ R/bpr.R
@@ -21,7 +21,7 @@
 #'
 #' @return the log likelihood
 #'
-#' @seealso \code{\link{bpr_derivative}}, \code{\link{design_matrix}}
+#' @seealso \code{\link{bpr_gradient}}, \code{\link{design_matrix}}
 #'
 #' @examples
 #' obj <- polynomial.object(M=2)
@@ -53,19 +53,19 @@ bpr_likelihood <- function(w, H, data, is_NLL = FALSE){
 }
 
 
-#' Derivative of the BPR log likelihood function
+#' Gradient of the BPR log likelihood function
 #'
-#' \code{bpr_derivative} computes the derivative w.r.t the coefficients w of
+#' \code{bpr_gradient} computes the gradient w.r.t the coefficients w of
 #'  the Binomial distributed Probit regression log likelihood function.
 #'
 #' @section Mathematical formula:
-#'  The derivative of the Binomial distributed Probit regression log likelihood
+#'  The gradient of the Binomial distributed Probit regression log likelihood
 #'  function w.r.t to w is computed by the following formula:
 #' \deqn{log p(y | f, w) = \sum_{l=1}^{L} log Binom(m_{l} | t_{l}, \Phi(w^{t}h(x_{l})))}
 #'
 #' @inheritParams bpr_likelihood
 #'
-#' @return the derivative vector of the log likelihood w.r.t the vector of
+#' @return the gradient vector of the log likelihood w.r.t the vector of
 #' coefficients w
 #'
 #' @seealso \code{\link{bpr_likelihood}}, \code{\link{design_matrix}}
@@ -77,10 +77,10 @@ bpr_likelihood <- function(w, H, data, is_NLL = FALSE){
 #' H <- des_mat$H
 #' w <- c(.1,.1,.1)
 #' data <- matrix(c(10,12,15,7,9,8), ncol=2)
-#' der <- bpr_derivative(w, H, data)
+#' gr <- bpr_gradient(w, H, data)
 #'
 #' @export
-bpr_derivative <- function(w, H, data, is_NLL = FALSE){
+bpr_gradient <- function(w, H, data, is_NLL = FALSE){
   total <- data[ ,1]
   succ  <- data[ ,2]
 
@@ -91,17 +91,90 @@ bpr_derivative <- function(w, H, data, is_NLL = FALSE){
   # Compute the density of a N(0,1) distribution
   N <- as.vector(dnorm(g))
 
-  # Compute the derivative vector w.r.t the coefficients w
+  # Compute the gradient vector w.r.t the coefficients w
   if (NROW(H) == 1){
-    der <- (succ * (1 / Phi) - (total - succ) * (1 / (1 - Phi))) * N %*% H
+    gr <- (succ * (1 / Phi) - (total - succ) * (1 / (1 - Phi))) * N %*% H
   }else{
-    der <- (t(succ) %*% diag(1 / Phi) - t(total - succ) %*%
-              diag(1 / (1 - Phi))) %*% diag(N) %*% H
+    gr <- (t(succ) %*% diag(1 / Phi) - t(total - succ) %*%
+             diag(1 / (1 - Phi))) %*% diag(N) %*% H
   }
 
   # If we required the Negative Log Likelihood
   if (is_NLL){
-    der <- (-1) * der
+    gr <- (-1) * gr
   }
-  return(der)
+  return(gr)
+}
+
+
+#' Sum of weighted BPR log likelihoods
+#'
+#' \code{sum_weighted_bpr_lik} computes the sum of the BPR log likelihoods for
+#' each elements of x, and then weights them by the corresponding posterior
+#' probabilities. This function is mainly used for the M-Step of the EM
+#' algorithm \code{\link{bpr_EM}}.
+#'
+#' @param w A vector of parameters (i.e. coefficients of the basis functions)
+#' @param x A list of elements of length N, where each element is an L x 3
+#'  matrix of observations, where 1st column contains the locations. The 2nd
+#'  and 3rd columns contain the total trials and number of successes at the
+#'  corresponding locations, repsectively.
+#' @param des_mat A list of length N, where each element contains the
+#'  \code{L x M} design matrices, where L is the number of observations and M
+#'  the number of basis functions.
+#' @param post_prob A vector of length N containing the posterior probabilities
+#'  fore each element of list x, respectively.
+#' @param is_NLL Logical, indicating if the Negative Log Likelihood should be
+#'  returned.
+#'
+#' @return The weighted sum of BPR log likelihoods
+#'
+#' @seealso \code{\link{bpr_likelihood}}, \code{\link{bpr_EM}}
+#'
+#' @export
+sum_weighted_bpr_lik <- function(w, x, des_mat, post_prob, is_NLL = TRUE){
+  N <- length(x)
+
+  # For each element in x, evaluate the BPR log likelihood
+  res <- vapply(X   = 1:N,
+                FUN = function(y) bpr_likelihood(w = w,
+                                                 H = des_mat[[y]]$H,
+                                                 data = x[[y]][ ,2:3],
+                                                 is_NLL = is_NLL),
+                FUN.VALUE = numeric(1),
+                USE.NAMES = FALSE)
+
+  # Return the dot product of the result and the posterior probabilities
+  return(post_prob %*% res)
+}
+
+
+#' Sum of weighted gradients of the BPR log likelihood
+#'
+#' \code{sum_weighted_bpr_grad} computes the sum of the gradients of BPR log
+#' likelihood for each elements of x, and then weights them by the
+#' corresponding posterior probabilities. This function is mainly used for the
+#' M-Step of the EM algorithm \code{\link{bpr_EM}}.
+#'
+#' @inheritParams sum_weighted_bpr_lik
+#'
+#' @return A vector with weighted sum of the gradients of BPR log likelihood.
+#'
+#' @seealso \code{\link{bpr_gradient}}, \code{\link{bpr_EM}}
+#'
+#' @export
+sum_weighted_bpr_grad <- function(w, x, des_mat, post_prob, is_NLL = TRUE){
+  N <- length(x)
+
+  # For each element in x, evaluate the gradient of the BPR log likelihood
+  res <- vapply(X   = 1:N,
+                FUN = function(y) bpr_gradient(w = w,
+                                               H = des_mat[[y]]$H,
+                                               data = x[[y]][ ,2:3],
+                                               is_NLL = is_NLL),
+                FUN.VALUE = numeric(length(w)),
+                USE.NAMES = FALSE)
+
+  # Return the dot product of the result and the posterior probabilities
+  return(post_prob %*% t(res))
 }
